f378645e9d5ad83adcb2a4b2474ba6d4629806ec
Andrew Do <andrewdough@gmail.com>
2013-09-26 02:36:26 -0700

    simple stuff

--- budar.php
+++ budar.php
@@ -1,2 +1,75 @@
 <?php
-namespace budar;
+namespace Budar;
+
+class Model
+{
+	// model settings
+	private $connection;
+	protected $table;
+	protected $primaryKey;
+
+	// callbacks
+	protected $beforeSave = array();
+	protected $afterSave = array();
+	protected $beforeCreate = array();
+	protected $afterCreate = array();
+
+	// relationships
+	protected $hasOne;
+	protected $hasMany;
+	protected $belongsTo;
+	protected $relData;
+
+	public function __construct($connectionName=null) {
+		if ($connectionName) {
+
+		} else {
+
+		}
+	}
+
+	public static function get() {
+
+	}
+}
+
+class Config
+{
+	private static $instance;
+	private $connections;
+	private $defaultConnection;
+
+	private function __construct() { }
+
+	/*
+	 *
+	 * @param array $connectionArr Associate array of PDO connection strings, or single 
+	 * @param string $defaultConnection Optional default connection
+	 */
+	public static function init($connections, $defaultConnection=null) {
+		if (self::$instance) {
+			// exception: already initialized
+		} else if (!is_array($connections) || count($connections) == 0) {
+			// exception: must define connections
+			return false;
+		} else if ($defaultConnection && count($connections) > 1 && !isset($connections[$defaultConnection])) {
+			// exception: must define default connection
+			return false;
+		} else {
+			self::$instance = new self();
+			self::$instance->connections = $connections;
+			self::$defaultConnection = $defaultConnection;
+			return true;
+		}
+	}
+
+	public static function instance() {
+		if (isset(self::$instance)) {
+			return self::$instance;
+		} else {
+			// exception: must initialize
+			return false;
+		}
+	}
+
+}
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+error_reporting(E_ALL);
+
+include 'budar.php';
+
+class MyModel extends Budar\Model
+{
+
+
+}
+
+Config::init(array('asdf'));
